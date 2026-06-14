73f7a29bf743e33ad35975e9e1883c349484ecec
Kirill Iost <kirilliost13@gmail.com>
2026-06-14 21:43:47 +0600

    Some clean code refactoring

--- script.pl
+++ script.pl
@@ -1,9 +1,3 @@
-# <= прибытие сообщения (в этом случае за флагом следует адрес отправителя)
-# => нормальная доставка сообщения
-# -> дополнительный адрес в той же доставке
-# ** доставка не удалась
-# == доставка задержана (временная проблема)
-
 #!/usr/bin/perl
 
 use v5.28;
@@ -13,15 +7,15 @@ use DBI;
 
 my $LOG_FILE_PATH = './maillog';
 
-    # timestamp, int_id, flag, id 
+# timestamp, int_id, flag, id 
 my $ARRIVAL_REGEXP = qr/
     ^(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2})
     \s(\w*\-\w*\-\w*)
-    \s\<\=\s.*
-    id\=(.*)
+    \s<=\s.*
+    id=(.*)
     /mx;
 
-    # timestamp, int_id, flag, address
+# timestamp, int_id, flag, address
 my $LOG_REGEXP = qr/
     ^(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2})
     \s(\w*\-\w*\-\w*)
@@ -29,37 +23,78 @@ my $LOG_REGEXP = qr/
     \s([\w\-\.]+@([\w-]+\.)+[\w-]{2,}|<>|:blackhole:)
     /mx;
 
-my $messageCount = 0;
-my $logCount = 0;
+my $STR_WITHOUT_DATETIME_REGEXP = qr/^(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2})\s/;
 
-open (fileHandler, "<", $LOG_FILE_PATH) or die $!;
+my $DATE_TIME_CUT_REGEXP = qr/(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(\w+-\w+-\w+)/;
 
-my $dbh = DBI->connect(
-    "dbi:Pg:dbname=${ENV{DB_NAME}};host=${ENV{DB_HOST}};port=${ENV{DB_PORT}}",
-    $ENV{DB_USER},
-    $ENV{DB_PASSWORD},
-    { RaiseError => 1, AutoCommit => 1 }
-);
-
-my $message_insert = $dbh->prepare(
-    "INSERT INTO message (created, id, int_id, str, status) VALUES (?, ?, ?, ?, NULL)" # status ?
-);
-
-my $log_insert = $dbh->prepare(
-    "INSERT INTO log (created, int_id, str, address) VALUES (?, ?, ?, ?)"
-);
-
-while (<fileHandler>){
-    (my $str = $_) =~ s/^(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2})\s//g;
-    if ($_ =~ $ARRIVAL_REGEXP) {
-        $message_insert->execute($1, $3, $2, $str);
-    } elsif ($_ =~ $LOG_REGEXP) {
-        $log_insert->execute($1, $2, $str, $3);
-    } elsif ($_ =~ /(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(\w+-\w+-\w+)/) { 
-        $log_insert->execute($1, $2, $str, undef);
+sub main {
+    my $FH = openFile($LOG_FILE_PATH) or die "Failed to open '$LOG_FILE_PATH' : $!";
+    unless (defined $FH) {
+        die "Failed to open '$LOG_FILE_PATH'";
     }
+
+    my $dbh = connectDB($ENV{DB_NAME}, $ENV{DB_HOST}, $ENV{DB_PORT}, $ENV{DB_USER}, $ENV{DB_PASSWORD});
+    
+    my $message_insert = $dbh->prepare(
+        "INSERT INTO message (created, id, int_id, str, status) VALUES (?, ?, ?, ?, NULL)"
+    );
+    my $log_insert = $dbh->prepare(
+        "INSERT INTO log (created, int_id, str, address) VALUES (?, ?, ?, ?)"
+    );
+
+    while (my $line = <$FH>) {
+        chomp $line;
+        my $str = $line;
+        $str =~ s/$STR_WITHOUT_DATETIME_REGEXP//;
+        if ($line =~ $ARRIVAL_REGEXP) {
+            inserIntoMessageTable($message_insert, $1, $3, $2, $str);
+        } elsif ($line =~ $LOG_REGEXP) {
+            inserIntoLogTable($log_insert, $1, $2, $str, $3);
+        } elsif ($line =~ $DATE_TIME_CUT_REGEXP) { 
+            inserIntoLogTable($log_insert, $1, $2, $str, undef);
+        }
+    }
+
+    $message_insert->finish if $message_insert;
+    $log_insert->finish if $log_insert;
+
+    $dbh->disconnect if defined $dbh;
+    close $FH;
 }
 
-close(fileHandler);
+sub inserIntoMessageTable {
+    my ($message_insert, $created, $id, $int_id, $str) = @_;
+
+    $message_insert->execute($created, $id, $int_id, $str);
+}
+
+sub inserIntoLogTable {
+    my ($log_insert, $created, $int_id, $str, $address) = @_;
+
+    $log_insert->execute($created, $int_id, $str, $address);
+}
+
+sub connectDB {
+    my ($DB_NAME, $DB_HOST, $DB_PORT, $DB_USER, $DB_PASSWORD) = @_;
+    my $dbh = DBI->connect(
+        "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        $DB_USER,
+        $DB_PASSWORD,
+        { RaiseError => 1, AutoCommit => 1 }
+    );
+    return $dbh;
+}
+
+sub openFile {
+    my ($path) = @_;
+    open my $fielHadler, '<', $path or do {
+        my $err = $!;
+        warn "Cannot open '$path': $err";
+        return;
+    };
+    return $fielHadler;
+}
+
+main();
 
 1;
